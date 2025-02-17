df6d751c42e83fbfd90ba5fbeaf3b48ed4137079
souli-a <soulimane.abdesslam@gmail.com>
2025-02-17 20:21:49 +0100

    feat: design system values added to tailwind config

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,26 +1,5 @@
-import { useState } from 'react';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="min-h-dvh flex flex-col items-center justify-center gap-5">
-      <h1 className="text-2xl">Counting App</h1>
-      <div className="flex flex-row gap-5">
-        <button
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded"
-          onClick={() => setCount(count => count - 1)}>
-          -1
-        </button>
-        <button
-          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded"
-          onClick={() => setCount(count => count + 1)}>
-          +1
-        </button>
-      </div>
-      <h3 className="text-lg">{count}</h3>
-    </div>
-  );
-}
+const App = () => {
+  return <div></div>;
+};
 
 export default App;
