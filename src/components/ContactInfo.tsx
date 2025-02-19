dadf17c2fa262321c6f664d5bfe21f330cbe8a80
souli-a <soulimane.abdesslam@gmail.com>
2025-02-19 01:00:49 +0100

    feat: added a click to copy contact info

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,15 +1,26 @@
 import { Mail, PhoneForwarded } from 'lucide-react';
+import copyButtonValue from '../utils/copyValueToClipboard';
 
 const ContactInfo = () => {
   return (
     <address className="not-italic flex flex-row gap-[var(--spacing-2xl)] text-body-desktop">
       <div className="flex flex-row gap-[var(--spacing-md)] items-center">
         <Mail color="var(--color-primary)" size={20} />
-        <button className="select-auto">example@example.com</button>
+        <button
+          onClick={copyButtonValue}
+          className="select-auto"
+          value="example@example.com">
+          example@example.com
+        </button>
       </div>
       <div className="flex flex-row gap-[var(--spacing-md)] items-center">
         <PhoneForwarded color="var(--color-primary)" size={20} />
-        <button className="select-auto">+33612345678</button>
+        <button
+          onClick={copyButtonValue}
+          className="select-auto"
+          value="+33612345678">
+          +33612345678
+        </button>
       </div>
     </address>
   );
