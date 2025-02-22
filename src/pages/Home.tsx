2e9f31ff80a1147babff8547a3bf2751b73d779b
souli-a <soulimane.abdesslam@gmail.com>
2025-02-22 22:58:42 +0100

    feat(Home): add title tag

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import Header from '../components/Header';
 const Home = () => {
   return (
     <div className="flex flex-col text-dark bg-light h-full w-full">
+      <title>Build Stone</title>
       <Header />
       <div className="h-full w-full flex justify-center items-center text-title-desktop-1">
         EMPTY AREA.
