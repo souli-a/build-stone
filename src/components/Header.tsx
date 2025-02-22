d62dd82e4c1f99505a4532fbbaa4249167f51a48
souli-a <soulimane.abdesslam@gmail.com>
2025-02-22 21:53:27 +0100

    feat(Header): add magnetic effect to the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,12 @@ import ContactInfo from './ContactInfo';
 const Header = () => {
   return (
     <header className="flex items-center justify-between p-[var(--spacing-4xl)]">
-      <a href="/" title="Build Stone Home Page">
+      <a
+        href="/"
+        className="magnetic-element"
+        title="Build Stone page d'accueil">
         <img
-          className="max-w-30 hover:opacity-90 active:scale-95 transition-all duration-150"
+          className="max-w-30"
           src="./images/build-stone-logo.svg"
           alt="Build Stone Logo"
         />
