d0956b4dc681ebc4bc1f29d38514728b06af7ec1
souli-a <soulimane.abdesslam@gmail.com>
2025-02-23 17:42:39 +0100

    feat(Home): add text content description

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Heart } from 'lucide-react';
 import ButtonCTA from '../components/ButtonCTA';
 import Header from '../components/Header';
 import PrimaryColorSpan from '../components/PrimaryColorSpan';
+import TextBodyDesktop from '../components/TextBodyDesktop';
 import Heading1 from './../components/Heading1';
 import Heading6 from './../components/Heading6';
 
@@ -11,21 +12,20 @@ const Home = () => {
       <title>Build Stone</title>
       <Header />
       <div className="w-full h-full flex justify-center items-center">
-        <section className="h-fit max-w-[60rem] flex flex-col gap-[var(--spacing-xl)] items-center">
+        <section className="h-fit max-w-[60rem] flex flex-col gap-[var(--spacing-4xl)] items-center">
           <div className="flex gap-[var(--spacing-md)] items-center w-fit">
             <Heart color="var(--color-pink)" size={20} strokeWidth={4} />
-            <Heading6>
-              150+ clients <PrimaryColorSpan>satisfaits</PrimaryColorSpan>
-            </Heading6>
+            <Heading6>150+ clients satisfaits</Heading6>
           </div>
-          <div>
-            <Heading1 className="text-balance text-center">
-              Une rénovation intérieure <br />
-              de <PrimaryColorSpan>qualité</PrimaryColorSpan> pour des résultats{' '}
-              <PrimaryColorSpan>durables</PrimaryColorSpan>
-            </Heading1>
-          </div>
-          <ButtonCTA className="mt-2 magnetic-element">
+          <Heading1 className="text-balance text-center mb-[-0.5rem] mt-[-0.5rem]">
+            Une rénovation intérieure <br />
+            de <PrimaryColorSpan>qualité</PrimaryColorSpan> pour des résultats{' '}
+            <PrimaryColorSpan>durables</PrimaryColorSpan>
+          </Heading1>
+          <TextBodyDesktop className="text-balance text-center opacity-85">
+            Une équipe expérimentée — Tous corps d'état — Depuis 2001
+          </TextBodyDesktop>
+          <ButtonCTA className="magnetic-element uppercase">
             OBTENEZ UN DEVIS GRATUIT — DÈS MAINTENANT
           </ButtonCTA>
         </section>
