c68092d760440e11920520da8324def94e01bfea
souli-a <soulimane.abdesslam@gmail.com>
2025-02-28 16:26:25 +0100

    feat: changing toaster lib

--- src/components/ContactInfo.tsx
+++ src/components/ContactInfo.tsx
@@ -1,26 +1,27 @@
+import { usePrefersColorScheme } from '@anatoliygatt/use-prefers-color-scheme';
 import { CircleCheck, Mail, PhoneForwarded } from 'lucide-react';
-import toast, { Toaster } from 'react-hot-toast';
+import { toast, Toaster } from 'sonner';
 import copyButtonValue from '../utils/copyValueToClipboard';
 import ClipboardButton from './ui/ClipboardButton';
 
 const ContactInfo = ({ className }: { className?: string }) => {
+  const preferredColorScheme = usePrefersColorScheme();
+  const isDarkColorSchemePreferred = preferredColorScheme === 'dark';
+
   const handleCopy = (e: React.MouseEvent<HTMLButtonElement>) => {
     copyButtonValue(e);
-    toast.success('Copié !', {
-      duration: 1000,
-      position: 'top-center',
+    toast('Copié', {
+      style: {
+        width: 'fit-content',
+        fontSize: 'var(--text-body-desktop)',
+        fontWeight: 'var(--font-weight-body-desktop)',
+        fontFamily: 'Unbounded',
+        boxShadow: 'none',
+        borderRadius: '0.8rem',
+      },
       icon: (
         <CircleCheck color="var(--color-green)" size={20} strokeWidth={3} />
       ),
-      removeDelay: 1000,
-      style: {
-        borderRadius: 'var(--border-radius-button)',
-        boxShadow: 'none',
-        border: '1px solid var(--color-grey-border)',
-        maxWidth: 'fit-content',
-        padding: 'var(--spacing-xl)',
-        backgroundColor: 'var(--color-light-grey)',
-      },
     });
   };
 
@@ -30,7 +31,6 @@ const ContactInfo = ({ className }: { className?: string }) => {
       className={`not-italic flex flex-row gap-[var(--spacing-2xl)] text-body-desktop font-body-desktop items-center ${
         className || ''
       }`}>
-      <Toaster gutter={10} />
       <ClipboardButton handleCopy={handleCopy} value="example@example.com">
         <Mail color="var(--color-primary)" size={20} strokeWidth={3} />
         example@example.com
@@ -43,6 +43,12 @@ const ContactInfo = ({ className }: { className?: string }) => {
         />
         +33612345678
       </ClipboardButton>
+      <Toaster
+        expand
+        visibleToasts={3}
+        duration={1500}
+        theme={isDarkColorSchemePreferred ? 'dark' : 'light'}
+      />
     </address>
   );
 };
