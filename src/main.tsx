f46fa6b8927b0f63721e1ef5bb3d8bc79eee62f3
souli-a <soulimane.abdesslam@gmail.com>
2025-03-10 11:10:40 +0100

    feat: add SEO meta tags

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { ReactLenis } from 'lenis/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Footer from './components/Footer';
@@ -8,13 +9,11 @@ import ProcessSteps from './pages/ProcessSteps';
 import ServicesOverview from './pages/ServicesOverview';
 import './styles/index.css';
 import './utils/magneticElement';
-import { ReactLenis } from 'lenis/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ReactLenis root>
       <div className="text-dark bg-light min-h-dvh dark:text-light dark:bg-dark">
-        <title>Build Stone</title>
         <Home />
         <AboutUs />
         <ServicesOverview />
